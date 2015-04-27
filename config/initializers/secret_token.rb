538aca1c3427c131aa6b4d50d83208a58e1bd406
xtunes <staff1312@xuanlvkeji.com>
2015-04-27 17:09:03 +0800

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prictice::Application.config.secret_token = 'f91a89c219c30c8f0f2e6a0a40e97067e2dfcd00f2c648225dbd074073da90d292563429a0c1703c402d8600e73095099f98f2a7f0eadfb49579911fe9b242cc'
